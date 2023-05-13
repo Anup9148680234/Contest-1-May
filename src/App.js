b7fa7d8006a6be8c925fc6d0527560df80aacb44
Anup9148680234 <anup220799@gmail.com>
2023-05-14 00:05:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  // Declare state variables
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  // Validation function
+  const validateInput = () => {
+    if (num1.trim() === '' || num2.trim() === '') {
+        if(num1.trim() === ''){
+            setError('Error: Num1 Cannot be Empty');
+            setSuccess('');
+            setResult('');
+        }
+        else if(num2.trim() === ''){
+            setError('Error: Num2 Cannot be Empty');
+            setSuccess('');
+            setResult('');
+        }
+      
+      return false;
+    }
+    if (!/^-?\d*\.?\d+$/.test(num1) || !/^-?\d*\.?\d+$/.test(num2)) {
+      setError('Error: Please enter valid numbers.');
+      setSuccess('');
+      setResult('');
+      return false;
+    }
+    return true;
+  };
+
+  // Arithmetic operations
+  const add = () => {
+    if (validateInput()) {
+      setResult(parseFloat(num1) + parseFloat(num2));
+      setError('');
+      setSuccess('Success : Your result is shown above!');
+    }
+  };
+
+  const subtract = () => {
+    if (validateInput()) {
+      setResult(parseFloat(num1) - parseFloat(num2));
+      setError('');
+      setSuccess('Success : Your result is shown above!');
+    }
+  };
+
+  const multiply = () => {
+    if (validateInput()) {
+      setResult(parseFloat(num1) * parseFloat(num2));
+      setError('');
+      setSuccess('Success : Your result is shown above!');
+    }
+  };
+
+  const divide = () => {
+    if (validateInput()) {
+      if (parseFloat(num2) === 0) {
+        setError('Error: Cannot divide by zero.');
+        setResult('');
+        setSuccess('');
+      } else {
+        setResult(parseFloat(num1) / parseFloat(num2));
+        setError('');
+        setSuccess('Success : Your result is shown above!');
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+     <h1>React Calculator</h1>
+      <div>
+        <input placeholder='Num 1' type="text" id="num1" value={num1} onChange={e => setNum1(e.target.value)} />
+      </div>
+      <div>
+        <input placeholder='Num 2' type="text" id="num2" value={num2} onChange={e => setNum2(e.target.value)} />
+      </div>
+      <div>
+        <button onClick={add}>+</button>
+        <button onClick={subtract}>-</button>
+        <button onClick={multiply}>*</button>
+        <button onClick={divide}>/</button>
+      </div>
+      <div id='messages'>
+        {result && <p style={{ color: 'green' }}>Result: {result}</p>}
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {success && <p style={{ color: 'green' }}>{success}</p>}
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
